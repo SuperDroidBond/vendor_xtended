eae27e19a4a7453020c726a2add9808f83ca7fee
razorloves <razorloves@gmail.com>
2019-03-12 01:10:19 -0500

    config: Remove duplicate keyguard.no_require_sim prop
    Added here:
    https://github.com/LineageOS/android_build/blob/lineage-16.0/target/product/full_base_telephony.mk
    
    Change-Id: If94b53efa9612bec8ef296a9d6df86add3b0f7c1

--- config/common.mk
+++ config/common.mk
@@ -20,7 +20,6 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.config.alarm_alert=Bright_morning.ogg
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    keyguard.no_require_sim=true \
     ro.url.legal=http://www.google.com/intl/%s/mobile/android/basic/phone-legal.html \
     ro.url.legal.android_privacy=http://www.google.com/intl/%s/mobile/android/basic/privacy.html \
     ro.com.android.wifi-watchlist=GoogleGuest \
