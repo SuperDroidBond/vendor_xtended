f58233c6b64924fabd147a9e32a30b0a742ea1c5
Luca Stefani <luca.stefani.ge1@gmail.com>
2019-07-25 18:05:40 +0200

    xtended: Enforce privapp-permissions whitelist
    Change-Id: I1ca080e5addcc0876d7346f3cf09db8e8af39312

--- config/common.mk
+++ config/common.mk
@@ -117,6 +117,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_COPY_FILES += \
     vendor/xtended/prebuilt/common/etc/permissions/privapp-permissions-xtended.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-xtended.xml
 
+# Enforce privapp-permissions whitelist
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.control_privapp_permissions=enforce
+
 # Required packages
 PRODUCT_PACKAGES += \
     CellBroadcastReceiver \
